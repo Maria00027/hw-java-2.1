144a2fe91c33f77e4bc3e33bf3e8a01132f96309
Maria Bistrova <luma000.27@gmail.com>
2022-02-25 00:39:26 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void Main(String[] args) {
+
+        int ticketPrice = 120; // цена билета
+        int bonus = 20; // одна бодусная миля
+
+        System.out.println("колличество бонусных миль за билет");
+        System.out.println(ticketPrice / bonus);
+    }
+}
